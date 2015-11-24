2c3f899b5eb4277dd7f968139db012af7b33f2a9
Kyungwook Tak <k.tak@samsung.com>
2015-11-24 17:05:25 +0900

    Fix & Refactor internal unit tests
    Change-Id: Ib273d952c21042b9fafb900cc6d3d36e6af69e8a
    Signed-off-by: Kyungwook Tak <k.tak@samsung.com>

---
+++ tests/pkcs12/test-common.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+#pragma once
+
+#include <string>
+
+namespace TestData {
+
+extern const std::string ServerPfxWithPassPath;
+extern const std::string ServerPfxWithPass2Path;
+extern const std::string ServerPfxWithoutPassPath;
+extern const std::string UserP12WithPassPath;
+extern const std::string ServerCertPemPath;
+extern const std::string CertCrtPath;
+extern const std::string InvalidCertCrtPath;
+
+extern const std::string ServerPfxPass;
+extern const std::string ServerPfx2Pass;
+extern const std::string UserP12Pass;
+
+}

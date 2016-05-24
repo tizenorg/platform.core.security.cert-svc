2d652595704d31f046857b9877996d4fbfe67c9e
sangwan.kwon <sangwan.kwon@samsung.com>
2016-05-24 18:08:53 +0900

    Apply cert-checker client library
    * If ocsp's validation fail(not revoked), cert-svc call cert-checker
    
    Change-Id: Iabb5e14e6c728de09688dbfdf4bf5f9c6630728b

--- vcore/vcore/SignatureValidator.cpp
+++ vcore/vcore/SignatureValidator.cpp
@@ -39,6 +39,8 @@
 
 #include <vcore/SignatureValidator.h>
 
+#include <cchecker/ocsp.h>
+
 using namespace ValidationCore::CertStoreId;
 
 namespace {
@@ -399,11 +401,9 @@ VCerr SignatureValidator::Impl::baseCheck(
 		LogError("XmlSec unknown exception : " << e.DumpToString());
 		return E_SIG_INVALID_FORMAT;
 	} catch (const Ocsp::Exception::Base &e) {
-		LogInfo("OCSP will be handled by cert-checker later. : " << e.DumpToString());
-		/*
-		 *  Don't care ocsp exception here.
-		 *  because exception case will be handled by cert-checker after app installed
-		 */
+		LogInfo("Launch cert-checker. : " << e.DumpToString());
+		if (cchecker_ocsp_request() != 0)
+			LogError("Load cert-checker failed.");
 	} catch (const std::exception &e) {
 		LogError("std exception occured : " << e.what());
 		return E_SIG_UNKNOWN;
@@ -457,11 +457,9 @@ VCerr SignatureValidator::Impl::baseCheckList(
 		LogError("XmlSec unknown exception : " << e.DumpToString());
 		return E_SIG_INVALID_FORMAT;
 	} catch (const Ocsp::Exception::Base &e) {
-		LogInfo("OCSP will be handled by cert-checker later. : " << e.DumpToString());
-		/*
-		 *  Don't care ocsp exception here.
-		 *  because exception case will be handled by cert-checker after app installed
-		 */
+		LogInfo("Launch cert-checker. : " << e.DumpToString());
+		if (cchecker_ocsp_request() != 0)
+			LogError("Load cert-checker failed.");
 	} catch (...) {
 		LogError("Unknown exception in SignatureValidator::checkList");
 		return E_SIG_UNKNOWN;
